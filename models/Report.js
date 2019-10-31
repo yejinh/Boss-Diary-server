e80049257b269bc2be5f39121ed7e3ccf0033c4e
Hyejin <wnr212@gmail.com>
2019-11-01 02:31:30 +0900

    Add mongoose schema

--- models/Report.js
+++ models/Report.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose');
 
-const reportSchema = new mongoose.Schema({
-
-});
+const reportSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    body: {
+      type: String,
+      required: true
+    },
+    templateName: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: { createdAt: 'created_at' }
+  }
+);
 
 module.exports = mongoose.model('Report', reportSchema);
--- models/User.js
+++ models/User.js
@@ -1,7 +1,36 @@
 const mongoose = require('mongoose');
 
+const ObjectId = mongoose.Schema.Types.ObjectId;
 const userSchema = new mongoose.Schema({
-
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  profilePhoto: {
+    type: String,
+    required: true
+  },
+  points: {
+    type: Number,
+    default: 0
+  },
+  reports: [
+    {
+      type: ObjectId,
+      ref: 'Report',
+      required: true
+    }
+  ],
+  templates: [
+    {
+      type: String,
+      required: true
+    }
+  ]
 });
 
 module.exports = mongoose.model('User', userSchema);
